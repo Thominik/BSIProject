4c62cf930db2ba498a05d938eaf55a94dd0f97a8
Thominik <dominikbednarz.ti@gmail.com>
2022-01-24 12:18:35 +0100

    99%

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_service/account.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  @Output() cancelLogin = new EventEmitter();
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  cancel() {
+    this.cancelLogin.emit(false);
+  }
+}
