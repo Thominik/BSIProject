9a6e09a2614bc272e3f1874331da2f6fde7f5dde
Thominik <dominikbednarz.ti@gmail.com>
2022-01-24 11:10:59 +0100

    Added login and register

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register([FromForm]RegisterDto registerDto)
+        {
+            if (await UserExist(registerDto.Username)) return BadRequest("Podany login jest zajęty!");
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                Password = registerDto.Password
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            
+            return new UserDto
+            {
+                Username = user.UserName,
+                Password = user.Password
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login([FromForm]LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Błędny login!");
+
+            if (loginDto.Password == user.Password)
+            {
+                return new UserDto
+                {
+                    Username = user.UserName,
+                    Password = user.Password
+                };
+            }
+            else
+            {
+                return Unauthorized("Błędne hasło!");
+            }
+        }
+
+
+        private async Task<bool> UserExist(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
--- API/Data/Migrations/20220124095850_InitialCreate.cs
+++ API/Data/Migrations/20220124095850_InitialCreate.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace API.Data.Migrations
 {
-    public partial class InitailCreate : Migration
+    public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -15,9 +14,8 @@ namespace API.Data.Migrations
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
-                    UserName = table.Column<string>(type: "TEXT", nullable: false),
-                    PasswordHash = table.Column<byte[]>(type: "BLOB", nullable: false),
-                    PasswordSalt = table.Column<byte[]>(type: "BLOB", nullable: false)
+                    UserName = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
                 },
                 constraints: table =>
                 {
